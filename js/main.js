816f34bba061ba47cc278de7918766a7d9fd04d0
YutongWong <844350043@qq.com>
2019-09-08 12:04:23 +1000

    main page done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-function filterFunction() {
+function filterFunction(me) {
     var input, filter, a, i;
-    input = document.getElementById("in");
-    filter = input.value.toLowerCase();
+    // input = document.getElementById("in");
+    filter = me.value.toLowerCase();
     a = document.getElementById("options").getElementsByTagName("a");
     // console.log(a[0].textContent);
     // console.log(a[1].innerText.toLowerCase);
@@ -16,16 +16,38 @@ function filterFunction() {
     }
 }
 
+
+function filterFunction2(me) {
+    var input, filter, a, i;
+    // input = document.getElementById("in");
+    filter = me.value.toLowerCase();
+    a = document.getElementById("options2").getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        value = a[i].textContent || a[i].innerText;
+        if(value.toLowerCase().indexOf(filter)>-1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+
 function nextq() {
     document.getElementById("first-q").style.display = "none";
     document.getElementById("nextbtn").style.display = "none";
     console.log(document.getElementById("header").classList.contains("header1"));
     document.getElementById("header").classList.toggle("header2");
-    
+    document.getElementById("second-q").style.display = "block";
+    // document.getElementById("sendit").style.display = "block";
     // document.getElementById("first-q").style.opacity = 0;
     // document.getElementById("first-q").style.transition = "opacity 3s ease-in-out";
 }
 
-function foo() {
-    console.log("me.id");
+function fillin(me) {
+    document.getElementById("in").value = me.textContent;
+}
+
+function fillin2(me) {
+    document.getElementById("in2").value = me.textContent;  
 }
